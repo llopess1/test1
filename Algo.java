7ab8492663404fd9e78e4942b4cde309b021b68b
llopes <luiss.lopes13@gmail.com>
2023-10-17 20:35:45 +0200

    Correcion codihp

--- Algo.java
+++ Algo.java
@@ -1,8 +1,10 @@
+import java.util.Scanner;
+
 public class Algo {
 
     public static void main(String[] args) {
         System.out.println("Ponga un numero:");
-        Scanner key = new(System.in);
+        Scanner key = new Scanner(System.in);
     }
     
 }
